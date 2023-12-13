ac9c8fa729df8321ef1eae93577533e2778b0c0c
Abdul Baroon <99256762+marvelkingself@users.noreply.github.com>
2023-12-13 12:55:31 +0530

    change

--- Assignment_2/index.js
+++ Assignment_2/index.js
@@ -12,27 +12,44 @@ $(document).ready(function() {
     }
     else if(inputValue==="simage"){
       setimage= "simage"
-      alert(setimage)
+      // alert(setimage)
     }
     else if(inputValue==="pimage"){
       setimage= "pimage"
-      alert(setimage)
+      // alert(setimage)
     }
+  });
+  $(".main").on("change", ".inputs input", function() {
+    var inputElement = $(this)[0];
+    var files = inputElement.files[0].name;
+    console.log(files)
+    $(this).closest(".line").find(".propertyImg").attr("src",`img/${files}`);
+    
   });
 
 
   $(".add").click(function() {
-    var newdata = ` <div class="mt-2">
-    <input class="border" type="file" id="myfile" name="myfile" multiple>
-    <select class="border" title="" name="property" id="primary" >
-      <option value="simage">Secondery Image</option>
-      <option value="pimage">primary Image</option>
-    </select>
-    <a  class="remove cursor-pointer"  >Remove</p>
-    </div>`;
+    var newdata = `
+    <div class=" mx-10 line border flex w-3/4 ">
+    <div class=" border ">
+      <img he src="w-[]"  class="propertyImg" alt="property image " height="100px" width="100px">
+    </div>
+    <div class="inputs mt-6  ">
+        <div class="mt-2">
+          <input class="border" type="file" id="myfile" name="myfile" multiple>
+          <select class="border" title="" name="property" id="primary">
+          <option value="simage">Secondery Image</option>
+            <option value="pimage">primary Image</option>
+            
+          </select>
+          
+        </div>
+      </div>
+      <a class="remove cursor-pointer mt-8 ms-4">Remove</p>
+  </div>`;
 
     var newElement = $(newdata);
-    newElement.appendTo(".inputs");
+    newElement.appendTo(".addmore");
   });
 
   $(".main").on("click", ".remove", function() {
